19893e251d1886cd2b9ef26172ca84349d6e1492
moncefbouazzaoui <moncefbouazzaoui@gmail.com>
2020-05-21 23:05:49 +0100

    [ADD] loading component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <template>
   <div id="app">
-    <!--<div class="container">-->
-      <!--<AddTodo />-->
-      <!--<FilterTodos />-->
-      <!--<Todos />-->
-    <!--</div>-->
     <section class="todoapp">
       <Header />
       <Todos />
       <Footer />
     </section>
+    <loading :active.sync="isLoading" :can-cancel="false" :is-full-page="true"></loading>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import  './assets/style.css';
 import Todos from './components/Todos.vue';
 import Header from './components/layout/Header.vue';
 import Footer from './components/layout/Footer.vue';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 export default {
   name: 'App',
   components: {
       Todos,
       Header,
-      Footer
-  }
+      Footer,
+      Loading
+  },
+  computed: mapGetters(['isLoading'])
 }
 </script>
 
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -2,36 +2,47 @@ import axios from 'axios';
 
 const state = {
     todos : [],
+    isLoading: true,
     filter: 'all'
 };
 const getters = {
     allTodos: (state) => state.todos,
     completedTodos: (state) => state.todos.filter(todo => todo.completed = true),
     uncompletedTodos: (state) => state.todos.filter(todo => todo.completed = false),
-    countTodos: (state) => state.todos.length
+    countTodos: (state) => state.todos.length,
+    isLoading: (state) => state.isLoading
 };
 const actions = {
     async fetchTodos({ commit }) {
-        console.log(12)
+        commit('startLoading');
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=3');
         commit('setTodos', response.data);
+        commit('endLoading');
     },
     async addTodo({ commit }, title) {
+        commit('startLoading');
         const response = await axios.post('https://jsonplaceholder.typicode.com/todos', {
             title, completed: false
         });
         commit('addTodo', response.data);
+        commit('endLoading');
     },
     async deleteTodo({ commit }, id) {
+        commit('startLoading');
         await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
         commit('deleteTodo', id);
+        commit('endLoading');
     },
     async updateTodo({ commit }, todo) {
+        commit('startLoading');
         await axios.put(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, todo);
         commit('updateTodo', todo);
+        commit('endLoading');
     }
 };
 const mutations = {
+    startLoading: (state) => ( state.isLoading = true ),
+    endLoading: (state) => ( state.isLoading = false ),
     setTodos: (state, todos) => ( state.todos = todos ),
     addTodo: (state, todo) => ( state.todos.unshift(todo)),
     deleteTodo: (state, id) => ( state.todos = state.todos.filter(todo => todo.id !== id)),
